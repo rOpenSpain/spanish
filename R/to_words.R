80d1f79063080859389fcd5c603148657f1fb555
Jose Manuel <vera.josemanuel@gmail.com>
2018-02-23 15:51:31 +0100

    tests for to_words

--- R/to_words.R
+++ R/to_words.R
@@ -17,14 +17,13 @@ dozens <- c("diez", "once", "doce", "trece", "catorce", "quince",
            "dieciseis", " diecisiete", "dieciocho", "diecinueve")
 names(dozens) <- 0:9
 
-
-tens <- c("veinte", "treinta", "cuarenta", "cincuenta", "sesenta", "setenta", "ochenta",
-          "noventa")
+tens <- c("veinte", "treinta", "cuarenta", "cincuenta", "sesenta",
+          "setenta", "ochenta", "noventa")
 names(tens) <- 2:9
 
 
-hundred <- c("ciento", "doscientos", "trescientos", "cuatrocientos", "quinientos", "seiscientos", "setecientos",
-          "ochocientos", "novecientos")
+hundred <- c("ciento", "doscientos", "trescientos", "cuatrocientos","quinientos",
+             "seiscientos", "setecientos","ochocientos", "novecientos")
 names(hundred) <- 1:9
 
 
@@ -34,51 +33,75 @@ len <- nchar(x)
 digits <- rev(strsplit(as.character(x), "")[[1]])
 
 
+clean <- function(x) {
+
+  x <- gsub("unomil", "mil", x )
+  x <- gsub("uno millones", "un millón", x )
+  x <- gsub("unociento", "ciento ", x )
+  x <- gsub("ciento y ", "ciento ", x )
+  x <- gsub("veinte y ", "veinti", x )
+  x <- gsub("veinti$", "veinte", x, perl = TRUE )
+  x <- gsub("veinte y $", "veinte", x, perl = TRUE )
+  x <- gsub(" NA ", " ", x)
+
+  return(x)
+
+}
+
+
 
 get_number <- function(x) {
 
       len <- nchar(x)
+
       digits <- rev(strsplit(as.character(x), "")[[1]])
 
-      if (len == 1) x <- as.vector(units[digits])
 
-      if (len == 2)
+      if (len == 1) {
+
+        x <- as.vector(units[digits])
+
+      } else if (len == 2) {
+
         if (x <= 19) x <- as.vector(dozens[digits[1]]) else x <- as.vector(paste0(tens[digits[2]]," y ", units[digits[1]]))
 
-      if (len == 3) x <- as.vector(paste0(hundred[digits[3]]," ", if (as.numeric(paste0(digits[2],digits[3])) <= 19) as.vector(dozens[digits[1]]) else as.vector(paste0(tens[digits[2]]," y ", units[digits[1]]))))
+      } else if (len == 3) {
 
-      if (len == 4) x <- as.vector(paste0(units[digits[4]],"mil ",hundred[digits[3]]," ", if (as.numeric(paste0(digits[2],digits[3])) <= 19) as.vector(dozens[digits[1]]) else as.vector(paste0(tens[digits[2]]," y ", units[digits[1]]))))
+        x <- as.vector(paste0(hundred[digits[3]]," ", if ((as.numeric(paste0(digits[2],digits[1])) <= 19) && digits[2]!=0 ) as.vector(dozens[digits[1]]) else as.vector(paste0(tens[digits[2]]," y ", units[digits[1]]))))
 
-      x <- gsub("unomil", "mil", x )
-      x <- gsub("unociento", "ciento ", x )
-      x <- gsub("veinte y ", "veinti", x )
-      x <- gsub("veinti$", "veinte", x, perl = TRUE )
-      x <- gsub("veinte y $", "veinte", x, perl = TRUE )
-      x <- gsub(" NA ", " ", x)
+      } else if (len == 4) {
+
+        x <- as.vector(paste0(units[digits[4]],"mil ",hundred[digits[3]]," ", if (as.numeric(paste0(digits[2],digits[3])) <= 19) as.vector(dozens[digits[1]]) else as.vector(paste0(tens[digits[2]]," y ", units[digits[1]]))))
+
+      }
 
 
-      return(x)
+
+
+      return(clean(x))
 
 }
 
 
 if (len < 5) {
-  get_number(x)
+  x <- get_number(x)
 } else if (len == 5) {
-  paste0(get_number(substr(x,1,2)),"mil ",get_number(substr(x,3,5)))
+  x <- paste0(get_number(substr(x,1,2)),"mil ",get_number(substr(x,3,5)))
 } else if (len == 6) {
-  paste0(get_number(substr(x,1,3)),"mil ",get_number(substr(x,4,6)))
+  x <- paste0(get_number(substr(x,1,3)),"mil ",get_number(substr(x,4,6)))
 } else if (len == 7) {
-  paste0(get_number(substr(x,1,1)), " millones ", get_number(substr(x,2,4)),"mil ",get_number(substr(x,5,7)))
+  x <- paste0(get_number(substr(x,1,1)), " millones ", get_number(substr(x,2,4)),"mil ",get_number(substr(x,5,7)))
 } else if (len == 8) {
-  paste0(get_number(substr(x,1,2)), " millones ", get_number(substr(x,3,5)),"mil ",get_number(substr(x,6,8)))
+  x <- paste0(get_number(substr(x,1,2)), " millones ", get_number(substr(x,3,5)),"mil ",get_number(substr(x,6,8)))
 } else if (len == 9) {
-  paste0(get_number(substr(x,1,3)), " millones ", get_number(substr(x,4,6)),"mil ",get_number(substr(x,7,9)))
+  x <- paste0(get_number(substr(x,1,3)), " millones ", get_number(substr(x,4,6)),"mil ",get_number(substr(x,7,9)))
 } else {
   stop(paste(x, "muy grande!"))
 }
 
 
+
+    return(clean(x))
 
 }
 
--- tests/testthat/test-spanish.R
+++ tests/testthat/test-spanish.R
@@ -25,3 +25,30 @@ test_that("to_number error message when unexpected string.", {
 
   expect_error(to_number("Esto no es un numero, ya ves."), "*unexpected input*")
 })
+
+
+test_that("to_words returns character vector from integer.", {
+  result <- to_words("1234")
+
+  expect_type(result, "character")
+})
+
+
+test_that("to_words returns a correct translation from integer.", {
+  result <- to_words(1234)
+  target <- "mil doscientos treinta y cuatro"
+  expect_equivalent(result, target)
+})
+
+
+test_that("to_words works ok with millions from integer.", {
+  result <- to_words(2482103)
+  target <- "dos millones cuatrocientos ochenta y dosmil ciento tres"
+  expect_equivalent(result, target)
+})
+
+
+test_that("to_words error message when unexpected number.", {
+
+  expect_error(to_words("Esto no es un numero, ya ves."), "*unexpected input*")
+})
