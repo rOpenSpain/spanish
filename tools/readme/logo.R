ebd565e0a38b86adb686cf45b5a19e8407f8477c
dieghernan <diego.hernangomezherrero@gmail.com>
2020-12-29 12:51:16 +0100

    Save code and last rebuild

--- tools/readme/logo.R
+++ tools/readme/logo.R
@@ -1,7 +1,7 @@
 library(hexSticker)
 # https://github.com/GuangchuangYu/hexSticker
 # Add font
-#sysfonts::font_add_google("Architects Daughter", "architect")
+sysfonts::font_add_google("Architects Daughter", "architect")
 
 imgurl <- "tools/readme/toro.png"
 sticker(
@@ -9,14 +9,32 @@ sticker(
   package = "spanish",
   p_size = 25,
   p_y =1.61,
-  s_x = 1,
-  s_y = .83,
-  s_width = .75,
+  s_x = 0.97,
+  s_y = .85,
+  s_width = .58,
   h_fill = "#f93740",
   h_color = "#fef34c",
   p_family = "architect",
   filename = "tools/readme/logo.png"
 )
 
+# To Readme
 
+sticker(
+  imgurl,
+  package = "spanish",
+  p_size = 25,
+  p_y =1.61,
+  s_x = 0.97,
+  s_y = .85,
+  s_width = .58,
+  h_fill = "#f93740",
+  h_color = "#fef34c",
+  p_family = "architect",
+  filename = "man/figures/logo.png"
+)
+
+# Favicons
+
+pkgdown::build_favicons(overwrite = TRUE)
 
